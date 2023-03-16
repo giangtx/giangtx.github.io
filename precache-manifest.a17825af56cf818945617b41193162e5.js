7a729316bb4d24e5c2f55dffefb8779f0ce2b43f
unknown <giangtx13@gmail.com>
2023-03-16 18:53:21 +0700

    Updates

---
+++ precache-manifest.a17825af56cf818945617b41193162e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f53ef547db48a7b96813",
+    "url": "/static/css/main.4fbffe9d.chunk.css"
+  },
+  {
+    "revision": "f53ef547db48a7b96813",
+    "url": "/static/js/main.9e8c2b4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d4282984429b71c3efac",
+    "url": "/static/js/2.e572f594.chunk.js"
+  },
+  {
+    "revision": "41f5e8ff1d98d490a19c6d48ea7b74b1",
+    "url": "/static/media/beyond_the_mountains.41f5e8ff.ttf"
+  },
+  {
+    "revision": "6b077b1b16edcce88aafcf81eef4e83b",
+    "url": "/index.html"
+  }
+];
